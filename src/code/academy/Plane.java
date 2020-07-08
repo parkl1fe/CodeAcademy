136ef2dbf3c35732c28574ee55c2540aa3f68d94
sebpra <sebpra@gmail.com>
2020-07-08 19:21:50 +0300

    Task 2 Done

--- src/code/academy/Plane.java
+++ src/code/academy/Plane.java
@@ -6,7 +6,11 @@ public class Plane {
 
     public void lowerChasis() {
         int randomNumber = new Random().nextInt(10);
-        int i = 4 / randomNumber;
-        System.out.println("OK: chasis lowered successfully");
+        try {
+            int i = 4 / randomNumber;
+            System.out.println("OK: chasis lowered successfully");
+        } catch (ArithmeticException e) {
+            System.out.println("ERROR: failed to lower chasis");
+        }
     }
 }
